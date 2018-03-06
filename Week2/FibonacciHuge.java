98fd2d15b15b45d4f800ebc8fa45a4da790fb872
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-03-06 23:34:49 +0800

    finish Huge Fib problem.

--- Week2/FibonacciHuge.java
+++ Week2/FibonacciHuge.java
@@ -18,11 +18,60 @@ public class FibonacciHuge {
         return current % m;
     }
     
+    private static long getFibHugeEffDirect(long n, long m) {
+    		// this one directly calcautes the mod without using the pisano period
+    		long tmp_previous = 0, previous = 1;
+    		long current = 0;
+    		for (long k=2; k<=n; k++) {
+    			current =  ((tmp_previous % m) + (previous % m)) % m;
+    			tmp_previous = previous;
+    			previous = current;
+    		}
+    		return current;
+    }
+    
+    private static long getFibHugeEffPisanoP(long n, long m) {
+    		// find next cycle which has {0,1}
+    		long tmp_previous = 1, previous = 1, current = 1;
+    		long k = 2;
+    		while (!(tmp_previous == 0) || !(previous == 1)) {
+    			k ++;
+    			current = ((tmp_previous % m) + (previous % m)) % m;
+    			tmp_previous = previous;
+    			previous = current;
+    		}
+    		k--;
+    		// since we find out the period, we can calcuate the answer
+    		long ans = getFibHugeEffDirect(n%k, m);
+    		return ans;
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
         long m = scanner.nextLong();
-        System.out.println(getFibonacciHugeNaive(n, m));
+        
+        System.out.println("This is from Naive :");
+        long startTime = System.nanoTime();
+        long ans = getFibonacciHugeNaive(n,m);
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("n=" + n + ",m=" + m + ", ans=" + ans);
+        System.out.println("elapsed time = " + elapsedTime + "ns");
+        
+        System.out.println("This is from DirectEff.");
+        startTime = System.nanoTime();
+        ans = getFibHugeEffDirect(n,m);
+        elapsedTime = System.nanoTime() - startTime;
+        System.out.println("n=" + n + ",m=" + m + ", ans=" + ans);
+        System.out.println("elapsed time = " + elapsedTime + "ns");
+        
+        System.out.println("This is from PisanoEff.");
+        startTime = System.nanoTime();
+        ans = getFibHugeEffPisanoP(n,m);
+        elapsedTime = System.nanoTime() - startTime;
+        System.out.println("n=" + n + ",m=" + m + ", ans=" + ans);
+        System.out.println("elapsed time = " + elapsedTime + "ns");
+        
     }
 }
 

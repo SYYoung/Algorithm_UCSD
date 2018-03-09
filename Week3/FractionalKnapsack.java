e3625a3cbcb84851253a2a4a4801058d36be8688
SY Young <r38411@yahoo.com>
2018-03-09 14:27:35 +0800

    start #2

--- Week3/FractionalKnapsack.java
+++ Week3/FractionalKnapsack.java
@@ -6,6 +6,17 @@ public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
         //write your code here
+		// A<-[0,0,...0], V<- 0
+		// repeat n times:
+		//		if W = 0:
+		//			return (V,A)
+		//		select i with wi > 0 and max vi/wi
+		//		a <- min(wi, W)
+		//		V <- V + a*vi/wi
+		//		wi <- wi-a, A[i]<-A[i]+a, W<-W-a
+		//	return (V,A)
+		// however, this algorithm can be improved asympt if the array is sorted
+		//  seletc i with wi>0 and max vi/wi can be removed. the array is: v1/w1 >= v2/w2>=...>=vn/Wn
 
         return value;
     }

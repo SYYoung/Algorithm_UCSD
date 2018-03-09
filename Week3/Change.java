90b771ba1ff1eed974f5f7cf4321b4f3a3202ff6
SY Young <r38411@yahoo.com>
2018-03-09 14:06:36 +0800

    finish #1: change problem

--- Week3/Change.java
+++ Week3/Change.java
@@ -1,11 +1,38 @@
 package Week3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Change {
-    private static int getChange(int m) {
+    private static int getChange(int amt) {
+		// A<-[0,0,...0], V<- 0
+		// repeat n times:
+		//		if W = 0:
+		//			return (V,A)
+		//		select i with wi > 0 and max vi/wi
+		//		a <- min(wi, W)
+		//		V <- V + a*vi/wi
+		//		wi <- wi-a, A[i]<-A[i]+a, W<-W-a
+		//	return (V,A)
+		// however, this algorithm can be improved asympt if the array is sorted
+		//  seletc i with wi>0 and max vi/wi can be removed. the array is: v1/w1 >= v2/w2>=...>=vn/Wn
         //write your code here
-        return m;
+    		int[] dom = {10, 5, 1};
+    		int leftOver = amt, totalV = 0, count = 0;
+    		for (int k=0; k<dom.length; k++) {
+    			if (leftOver == 0) {
+    				break;
+    			}
+    			else {
+    				int num = leftOver/dom[k];
+    				count += num;
+    				totalV += dom[k] * num;
+    				leftOver -= dom[k] * num;
+    				System.out.println("count=" + count + ", totalV = "+ totalV +
+    						",  leftOver= " + leftOver);
+    			}
+    		}
+        return count;
     }
 
     public static void main(String[] args) {

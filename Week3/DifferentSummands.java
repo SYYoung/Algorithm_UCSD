8c21775a6856c6f79a2d1f543f92ce8adac1e24c
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-03-12 22:57:12 +0800

    finish #5

--- Week3/DifferentSummands.java
+++ Week3/DifferentSummands.java
@@ -4,7 +4,23 @@ import java.util.*;
 
 public class DifferentSummands {
     private static List<Integer> optimalSummands(int n) {
+		// R<- {}, i<-1
+		// while i<=n:
+		//		[l,r] <- [xi, xi+1] unit length
+		//		i <- i + 1;
+		//		try to find out the next leftmost point after this unit segment
+		//		while i<=n and xi <= r
+		//			i <- i+1
+		// return R
         List<Integer> summands = new ArrayList<Integer>();
+        int curr = 0;
+        int leftOver = n;
+        while (leftOver >= 2*curr + 3) {
+        		curr += 1;
+        		leftOver -= curr;
+        		summands.add(curr);
+        }
+        summands.add(leftOver);
         //write your code here
         return summands;
     }

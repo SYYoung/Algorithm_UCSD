059de22c84db4dc7f982ff1a1e74bdeeea6e40e3
SY Young <r38411@yahoo.com>
2018-03-12 14:24:29 +0800

    start to work #4

--- Week3/CoveringSegments.java
+++ Week3/CoveringSegments.java
@@ -4,34 +4,56 @@ import java.util.*;
 
 public class CoveringSegments {
 
-    private static int[] optimalPoints(Segment[] segments) {
+    private static int[] optimalPoints(ArrayList<Segment> segments) {
         //write your code here
-        int[] points = new int[2 * segments.length];
-        for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
-        }
-        return points;
+		// assume that the arrays is sorted
+		// R<- {}, i<-1
+		// while i<=n:
+		//		[l,r] <- [xi, xi+1] unit length
+		//		i <- i + 1;
+		//		try to find out the next leftmost point after this unit segment
+		//		while i<=n and xi <= r
+		//			i <- i+1
+		// return R
+    	
+    		int[] points = new int[8];
+    		return points;
     }
 
-    private static class Segment {
+    private static class Segment implements Comparable<Segment> {
         int start, end;
 
         Segment(int start, int end) {
             this.start = start;
             this.end = end;
         }
+        
+        public boolean isBetween(int point) {
+        		if ((point >= this.start) && (point <= this.end)) {
+        			return true;
+        		}
+        		return false;
+        }
+
+		public int compareTo(Segment seg1, Segment seg2) {
+			return seg1.end < seg2.end ? -1 : seg1.end == seg2.end ? 0 : 1;
+		}
     }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        Segment[] segments = new Segment[n];
+        //Segment[] segments = new Segment[n];
+        ArrayList<Segment> segments = new ArrayList<Segment>();
         for (int i = 0; i < n; i++) {
             int start, end;
             start = scanner.nextInt();
             end = scanner.nextInt();
-            segments[i] = new Segment(start, end);
+            segments.add(new Segment(start, end));
         }
+        
+        // call the sorting endpoints
+        Collections.sort(segments);
         int[] points = optimalPoints(segments);
         System.out.println(points.length);
         for (int point : points) {

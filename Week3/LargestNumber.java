496512ef165caa6f7022510135cc849cc8c95be9
SY Young <r38411@yahoo.com>
2018-03-13 12:03:06 +0800

    add stress test of #6

--- Week3/LargestNumber.java
+++ Week3/LargestNumber.java
@@ -28,7 +28,30 @@ public class LargestNumber {
     		return result;
     }
 
+	public static void StressTest() {
+		// n is between 1 and 100
+		// those numbers are between 1 and 1000
+		int num = (int)(Math.random() * 10);
+		if (num ==0)
+			num = 1;
+		// generate those numbers
+		String[] a = new String[num];
+		for (int k=0; k<num; k++) {
+			int val;
+			val = (int)(Math.random() * 100);
+			a[k] = String.valueOf(val);
+		}
+		//display the numbers
+		System.out.println("number generated: " + num);
+		for (String s : a) {
+			System.out.print("\t" + s);
+		}
+		System.out.println();
+		System.out.println(largestNumber(a));
+	}
+	
     public static void main(String[] args) {
+    	/*
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         String[] a = new String[n];
@@ -36,6 +59,8 @@ public class LargestNumber {
             a[i] = scanner.next();
         }
         System.out.println(largestNumber(a));
+        */
+        StressTest();
     }
 }
 

7ba233cbe759cfc316320d140e3d47ff6660d4a8
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-03-16 17:55:27 +0800

    finish binary search

--- Week4/BinarySearch.java
+++ Week4/BinarySearch.java
@@ -6,9 +6,18 @@ import java.util.*;
 public class BinarySearch {
 
     static int binarySearch(int[] a, int x) {
-        int left = 0, right = a.length;
-        //write your code here
-
+        int left = 0, right = a.length-1;
+        while (left <= right) {
+        		int middle = (left + right)/2;
+        		if (x == a[middle]) 
+        			return middle;
+        		else if (x < a[middle]) {
+        			right = middle - 1;
+        		}
+        		else {
+        			left = middle + 1;
+        		}		
+        }
         return -1;
     }
 
@@ -18,8 +27,51 @@ public class BinarySearch {
         }
         return -1;
     }
+    
+    public static void stressTest() {
+    		int n = new Random().nextInt(20);
+    		if (n==0)
+    			n = 1;
+    		System.out.println(" num generated: " + n);
+    		int[] a = new int[n];
+    		for (int k=0; k<n; k++) {
+    			a[k] = new Random().nextInt(10000);
+    		}
+    		System.out.println("before sorting:");
+    		for (int k: a)
+    			System.out.print("\t " + k);
+    		System.out.println("\n. After sorting:");
+    		Arrays.sort(a);
+    		for (int k: a)
+    			System.out.print("\t " + k);
+    		
+    		// now b
+    		int key  = new Random().nextInt(n);
+    		if (key == 0)
+    			key = 1;
+    		System.out.println("\n# of keys is : " + key);
+    		int[] b = new int[key];
+    		for (int k=0; k<key; k++) {
+    			b[k] = a[(new Random().nextInt(n))];
+    		}
+    		b[key-1] = new Random().nextInt(10000);
+    		System.out.println("\t. the keys are: ");
+    		for (int k: b) {
+    			System.out.print("\t " + k);
+    		}
+    		
+    		System.out.println("\nLinear search result: ");
+    		for (int k=0; k<key; k++) {
+    			System.out.print(linearSearch(a, b[k]) + " ");
+    		}
+    		System.out.println("\n Binary search : ");
+    		for (int k=0; k<key; k++) {
+    			System.out.print(binarySearch(a, b[k]) + " ");
+    		}
+    }
 
     public static void main(String[] args) {
+    		/*
         FastScanner scanner = new FastScanner(System.in);
         int n = scanner.nextInt();
         int[] a = new int[n];
@@ -35,7 +87,11 @@ public class BinarySearch {
             //replace with the call to binarySearch when implemented
             System.out.print(linearSearch(a, b[i]) + " ");
         }
+        */
+    	
+        stressTest();
     }
+    
     static class FastScanner {
         BufferedReader br;
         StringTokenizer st;

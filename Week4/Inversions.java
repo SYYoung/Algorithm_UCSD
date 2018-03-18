479cc2e211232f03358da0e8427980e6d816f71b
Ki Ling Cheung <kilingcheung@Kis-MacBook-Pro.local>
2018-03-18 23:25:38 +0800

    working on #4:inversion

--- Week4/Inversions.java
+++ Week4/Inversions.java
@@ -6,15 +6,62 @@ public class Inversions {
 
     private static long getNumberOfInversions(int[] a, int[] b, int left, int right) {
         long numberOfInversions = 0;
-        if (right <= left + 1) {
+        if (right <= left) {
+        		b[0] = a[right];
             return numberOfInversions;
         }
-        int ave = (left + right) / 2;
-        numberOfInversions += getNumberOfInversions(a, b, left, ave);
-        numberOfInversions += getNumberOfInversions(a, b, ave, right);
+        int ave = left + (right - left) / 2;
+        int[] b1 = new int[ave-left+1];
+        int[] b2 = new int[right-(ave+1)+1];
+        numberOfInversions += getNumberOfInversions(a, b1, left, ave);
+        numberOfInversions += getNumberOfInversions(a, b2, ave+1, right);
         //write your code here
+        numberOfInversions += getMerge(b1, b2, b, left, right);
         return numberOfInversions;
     }
+    
+	public static int getMerge(int[]B, int[]C, int[] APrime, int left, int right) {
+		// D <- empty array of size p+q
+		// while B and C are both non-empty:
+		//		b <- the first element of B
+		//		c <- the first element of C
+		//		if b<=c:
+		//			move b from B to the end of D
+		//		else:
+		//			move c from C to the end of D
+		//	move the rest of B and C to the end of D
+		//	return D		
+		int bIndex = 0, cIndex = 0, curr = 0;
+		int countInverse = 0;
+		while ((bIndex < B.length) && (cIndex < C.length)) {
+			if (B[bIndex] <= C[cIndex]) {
+				APrime[curr] = B[bIndex];
+				bIndex++;
+				curr++;
+			}
+			else {
+				APrime[curr] = C[cIndex];
+				// for those after bIndex, they are all inversions
+				countInverse = (B.length - 1 - bIndex) + 1;
+				cIndex++;
+				curr++;
+			}
+		}
+		if (bIndex == B.length) {
+			for (int k=cIndex; k < C.length; k++) {
+				APrime[curr] = C[k];
+				curr++;
+			}	
+		}
+		else if (cIndex == C.length) {
+			for (int k=bIndex; k<B.length; k++) {
+				APrime[curr] = B[k];
+				curr++;
+			}
+		}
+		return countInverse;
+	}
+	
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -23,8 +70,12 @@ public class Inversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int[] b = new int[n];
-        System.out.println(getNumberOfInversions(a, b, 0, a.length));
+        
+        int[] b = new int[a.length];
+        System.out.println(getNumberOfInversions(a, b, 0, a.length-1));
+        for (int k=0; k<a.length; k++) {
+        		System.out.print("\t " + a[k]);
+        }
     }
 }
 
